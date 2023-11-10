3d9a216654dbed3964a3e77861a4028932d03f35
Kaloyan Georgiev <kaloyan.georgiev01@sap.com>
2023-11-10 09:13:36 +0200

    Initial - same as Sales sample project

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import com.example.hadoop.RunnerClass;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Starting application...");
+        RunnerClass.run("b");
+    }
+}
---
+++ src/main/java/com/example/hadoop/RunnerClass.java
@@ -0,0 +1,58 @@
+package com.example.hadoop;
+
+import java.io.IOException;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URI;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.RunningJob;
+import org.apache.hadoop.mapreduce.v2.app.webapp.App;
+
+public class RunnerClass {
+
+	public static void run(String inp) {
+		Configuration conf = new Configuration();
+
+		JobConf job = new JobConf(conf, App.class);
+
+		job.set("country", inp);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(DoubleWritable.class);
+		job.setMapperClass(MapperClass.class);
+		job.setReducerClass(ReducerClass.class);
+
+		Path input = new Path("hdfs://127.0.0.1:9000/uni_input/SalesJan2009.csv");
+		// Path output = new Path("hdfs://127.0.0.1:9000/uni_output/sales_result");
+		Path output = new Path("output");
+
+		FileInputFormat.setInputPaths(job, input);
+		FileOutputFormat.setOutputPath(job, output);
+
+		try {
+			FileSystem fs = FileSystem.get(URI.create("hdfs://127.0.0.1:9000"), conf);
+
+			if (fs.exists(output))
+				fs.delete(output, true);
+
+			// throws here
+			RunningJob task = JobClient.runJob(job);
+
+			System.out.println("Is successful: " + task.isSuccessful());
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
